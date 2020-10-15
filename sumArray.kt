29c7aa6f9417f70e823289c55c6190ec328a34c8
louiscahyayanuar <71306609+louiscahyayanuar@users.noreply.github.com>
2020-10-15 21:31:17 +0700

    Add files via upload

---
+++ sumArray.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val array1 : Array<Int> = arrayOf(1,2,3)
+    val array2 : Array<Int> = arrayOf(3,1,2)
+    val hasil = array1[0] + array2[0]
+    val hasil1 = array1[1] + array2[1]
+    val hasil2 = array1[2] + array2[2]
+    println("[$hasil, $hasil1, $hasil2]")
+}
